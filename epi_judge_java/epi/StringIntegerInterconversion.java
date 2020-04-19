28304a5ae0b861612a37fc4ccfda6483d471247e
Ismaaeel Ataullah <ismaaeelataullah@hotmail.com>
2020-04-19 23:28:01 +0200

    Q6.1

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -3,15 +3,53 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
-
+/*
+USEFUL: https://stackoverflow.com/questions/5444611/math-abs-returns-wrong-value-for-integer-min-value
+ */
   public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "0";
+    boolean isNegative = false;
+    if(x == 0)
+    {
+      return "0";
+    }
+    if (x < 0) {
+      isNegative = true;
+    }
+    String intToReturn = "";
+    while (x != 0) {
+      int lastDigit = Math.abs(x % 10);
+      char lastDigitChar = (char) (lastDigit + '0');
+      intToReturn = lastDigitChar + intToReturn;
+      x = x/10;
+    }
+
+    return isNegative ? "-" + intToReturn : intToReturn;
+
   }
+
   public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
+    int result = 0;
+    int multiplier = 1;
+    boolean isNegative = false;
+    if(s.charAt(0) == '-'){
+      isNegative = true;
+      s = s.substring(1);
+    }
+    if(s.charAt(0) == '+'){
+      s=s.substring(1);
+    }
+
+    for(int i = s.length()-1; i >= 0; i--)
+    {
+      char number = s.charAt(i);
+      int numberInt = Character.getNumericValue(number);
+      numberInt = numberInt*multiplier;
+      result = result + numberInt;
+      multiplier = multiplier*10;
+    }
+    return isNegative ? -result : result;
   }
+
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
   public static void wrapper(int x, String s) throws TestFailure {
     if (Integer.parseInt(intToString(x)) != x) {
