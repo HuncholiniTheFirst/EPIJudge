393ad58bd4ef27319c7a3a44a073d1ed816a1a46
Ismaaeel Ataullah <ismaaeelataullah@hotmail.com>
2020-04-26 11:49:00 +0200

    Q7.1

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -6,8 +6,31 @@ public class SortedListsMerge {
   //@include
   public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                       ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+   ListNode<Integer> result = new ListNode<>(null,null);
+   ListNode<Integer> current = result;
+
+   while(L1 != null)
+   {
+     Integer l1Val = L1.data;
+     Integer l2Val = L2 == null ? null : L2.data;
+
+     if(l2Val == null || l1Val <= l2Val)
+     {
+       current.next = L1;
+       L1 = L1.next;
+     }
+     else{
+       current.next = L2;
+       L2 = L2.next;
+     }
+
+     current = current.next;
+   }
+
+   if(L2 != null){
+     current.next = L2;
+   }
+    return result.next;
   }
 
   public static void main(String[] args) {
