1c5a45e59a45c1235f87e360921892e48832b125
Ismaaeel Ataullah <ismaaeelataullah@hotmail.com>
2020-04-28 15:41:12 +0200

    Q8.1

--- epi_judge_java/epi/StackWithMax.java
+++ epi_judge_java/epi/StackWithMax.java
@@ -1,4 +1,5 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
@@ -6,85 +7,106 @@ import epi.test_framework.TestFailure;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+
 public class StackWithMax {
 
-  public static class Stack {
-    public boolean empty() {
-      // TODO - you fill in here.
-      return true;
-    }
-    public Integer max() {
-      // TODO - you fill in here.
-      return 0;
-    }
-    public Integer pop() {
-      // TODO - you fill in here.
-      return 0;
-    }
-    public void push(Integer x) {
-      // TODO - you fill in here.
-      return;
-    }
-  }
-  @EpiUserType(ctorParams = {String.class, int.class})
-  public static class StackOp {
-    public String op;
-    public int arg;
+    private static class StackEntry {
+        int entryValue;
+        int maxSoFar;
 
-    public StackOp(String op, int arg) {
-      this.op = op;
-      this.arg = arg;
-    }
-  }
-
-  @EpiTest(testDataFile = "stack_with_max.tsv")
-  public static void stackTester(List<StackOp> ops) throws TestFailure {
-    try {
-      Stack s = new Stack();
-      int result;
-      for (StackOp op : ops) {
-        switch (op.op) {
-        case "Stack":
-          s = new Stack();
-          break;
-        case "push":
-          s.push(op.arg);
-          break;
-        case "pop":
-          result = s.pop();
-          if (result != op.arg) {
-            throw new TestFailure("Pop: expected " + String.valueOf(op.arg) +
-                                  ", got " + String.valueOf(result));
-          }
-          break;
-        case "max":
-          result = s.max();
-          if (result != op.arg) {
-            throw new TestFailure("Max: expected " + String.valueOf(op.arg) +
-                                  ", got " + String.valueOf(result));
-          }
-          break;
-        case "empty":
-          result = s.empty() ? 1 : 0;
-          if (result != op.arg) {
-            throw new TestFailure("Empty: expected " + String.valueOf(op.arg) +
-                                  ", got " + String.valueOf(s));
-          }
-          break;
-        default:
-          throw new RuntimeException("Unsupported stack operation: " + op.op);
+        public StackEntry(int value, int maxSoFar) {
+            this.entryValue = value;
+            this.maxSoFar = maxSoFar;
         }
-      }
-    } catch (NoSuchElementException e) {
-      throw new TestFailure("Unexpected NoSuchElement exception");
     }
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "StackWithMax.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static class Stack {
+        private java.util.Stack<StackEntry> underlyingStack;
+
+        public Stack() {
+            this.underlyingStack = new java.util.Stack<>();
+        }
+
+        public boolean empty() {
+            return this.underlyingStack.isEmpty();
+        }
+
+        public Integer max() {
+            return underlyingStack.empty() ? Integer.MIN_VALUE : underlyingStack.peek().maxSoFar;
+        }
+
+        public Integer pop() {
+            StackEntry pop = underlyingStack.pop();
+            return pop.entryValue;
+        }
+
+        public void push(Integer x) {
+            int newEntryMax = x > max() ? x : max();
+            StackEntry newEntry = new StackEntry(x, newEntryMax);
+            this.underlyingStack.push(newEntry);
+        }
+    }
+
+    @EpiUserType(ctorParams = {String.class, int.class})
+    public static class StackOp {
+        public String op;
+        public int arg;
+
+        public StackOp(String op, int arg) {
+            this.op = op;
+            this.arg = arg;
+        }
+    }
+
+    @EpiTest(testDataFile = "stack_with_max.tsv")
+    public static void stackTester(List<StackOp> ops) throws TestFailure {
+        try {
+            Stack s = new Stack();
+            int result;
+            for (StackOp op : ops) {
+                switch (op.op) {
+                    case "Stack":
+                        s = new Stack();
+                        break;
+                    case "push":
+                        s.push(op.arg);
+                        break;
+                    case "pop":
+                        result = s.pop();
+                        if (result != op.arg) {
+                            throw new TestFailure("Pop: expected " + String.valueOf(op.arg) +
+                                    ", got " + String.valueOf(result));
+                        }
+                        break;
+                    case "max":
+                        result = s.max();
+                        if (result != op.arg) {
+                            throw new TestFailure("Max: expected " + String.valueOf(op.arg) +
+                                    ", got " + String.valueOf(result));
+                        }
+                        break;
+                    case "empty":
+                        result = s.empty() ? 1 : 0;
+                        if (result != op.arg) {
+                            throw new TestFailure("Empty: expected " + String.valueOf(op.arg) +
+                                    ", got " + String.valueOf(s));
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported stack operation: " + op.op);
+                }
+            }
+        } catch (NoSuchElementException e) {
+            throw new TestFailure("Unexpected NoSuchElement exception");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "StackWithMax.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
