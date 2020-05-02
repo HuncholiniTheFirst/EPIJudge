4015b1e3c28ec59beacbe33c6faae4dbdf8cc669
Ismaaeel Ataullah <ismaaeelataullah@hotmail.com>
2020-05-02 18:16:56 +0200

    Q9.1 - Is tree Balanced

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,39 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
+        int result = isTreeBalanced(tree, 0);
+        return result == -1 ? false : true;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int isTreeBalanced(BinaryTreeNode<Integer> tree, int currentHeight) {
+        if (tree == null) {
+            return currentHeight;
+        }
+
+        int leftTree = isTreeBalanced(tree.left, currentHeight);
+        int rightTree = isTreeBalanced(tree.right, currentHeight);
+
+        if (leftTree == -1 || rightTree == -1) {
+            return -1;
+        }
+        if (Math.abs(leftTree - rightTree) > 1) {
+            return -1;
+        } else return (Math.max(leftTree, rightTree) + 1);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeBalanced.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
