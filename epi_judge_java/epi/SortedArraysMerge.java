880a2c379d07a6895434963da33e07e15cde2027
Ismaaeel Ataullah <ismaaeelataullah@hotmail.com>
2020-05-16 19:06:35 +0200

    Q10.1 - Sorted Arrays Merge

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -1,22 +1,60 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
+
 public class SortedArraysMerge {
-  @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+    public static class ArrayEntry {
 
-  public static List<Integer>
-  mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
-  }
+        public ArrayEntry(int value, int arrayID) {
+            this.value = value;
+            this.arrayID = arrayID;
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortedArraysMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int value;
+        int arrayID;
+    }
+
+    @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+
+
+    public static List<Integer> mergeSortedArrays(List<List<Integer>> sortedArrays) {
+        List<Integer> result = new ArrayList<>();
+        List<Iterator<Integer>> iterators = new ArrayList<>();
+        for (List<Integer> sortedArray : sortedArrays) {
+            iterators.add(sortedArray.iterator());
+        }
+
+        PriorityQueue<ArrayEntry> minHeap = new PriorityQueue<>(sortedArrays.size(), Comparator.comparingInt(a -> a.value));
+
+        //initialise heap
+
+        for (int i = 0; i < iterators.size(); i++) {
+            if (iterators.get(i).hasNext()) {
+                minHeap.add(new ArrayEntry(iterators.get(i).next(), i));
+            }
+        }
+
+        while (!minHeap.isEmpty()) {
+            ArrayEntry poll = minHeap.poll();
+            result.add(poll.value);
+            if (iterators.get(poll.arrayID).hasNext()) {
+                Integer next = iterators.get(poll.arrayID).next();
+                minHeap.add(new ArrayEntry(next, poll.arrayID));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortedArraysMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
